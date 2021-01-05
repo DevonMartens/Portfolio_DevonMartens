7bdbb836fbe4f7d2ff56f829a7119b9bd07a6ebb
DevonMartens <dmmartens91@gmail.com>
2021-01-05 12:16:42 -0500

    added onclick functions for if the buttons in the navbar are clicked

---
+++ assets/js/button.js
@@ -0,0 +1,34 @@
+//onclick function for if about is clicked
+$(".about-btn").on("click", (event) => {
+    event.preventDefault();
+    // scrolls to about me
+    $('html, body').animate({
+        scrollTop: $(".aboutme").offset().top - 87
+    }, 1000);
+})
+//onclick function for if projects is clicked
+$(".projects-btn").on("click", (event) => {
+    event.preventDefault();
+    // scrolls to portfolio element
+    $('html, body').animate({
+        scrollTop: $(".portfolio").offset().top
+    }, 1000);
+})
+//onclick function for if contact is clicked
+$(".contact-btn").on("click", (event) => {
+    event.preventDefault();
+    // scrolls to contact element
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top - 87
+    }, 1000);
+})
+
+//onclick function for if home is clicked
+$(".home-btn").on("click", (event) => {
+    event.preventDefault();
+    // scrolls to contact element
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+})
+
