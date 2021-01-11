b80306d0dcb343ce978647b168ffa567c61f3bac
DevonMartens <dmmartens91@gmail.com>
2021-01-11 10:50:33 -0500

    changes image on click

---
+++ assets/js/viewWork.js
@@ -0,0 +1,24 @@
+$('.view-work-btn').hover(function () {
+    let mobile = window.matchMedia("(min-width: 767px)")
+    let supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+    if (mobile.matches || supportsTouch === false) {
+       $('.banner-image2').css('opacity', '1');
+       $('.typewrite').css('color', 'blue');
+       $('.banner-title h1').css('color', 'blue');
+       $('.banner-title .btn').removeClass("btn-secondary");
+       $('.banner-title .btn').addClass("btn-primary");
+       $('.navbar').removeClass("navbar-dark");
+       $('.navbar').addClass("navbar-light");
+       $('.banner-name').css("color", "rgb(255, 102, 0)")
+    }
+ },
+    function () {
+       $('.banner-img2').css('opacity', '0');
+       $('.typewrite').css('color', 'white');
+       $('.banner-title h1').css('color', 'white');
+       $('.banner-title .btn').removeClass("btn-primary");
+       $('.banner-title .btn').addClass("btn-secondary");
+       $('.navbar').removeClass("navbar-light");
+       $('.navbar').addClass("navbar-dark");
+       $('.banner-name').css("color", "rgb(0, 140, 255)")
+    });
