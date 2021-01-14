870b748175b6bc02f603cd41308b15b08f60f43c
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-01-14 13:19:24 -0500

    background test

---
+++ assets/js/portfolio.js
@@ -0,0 +1,38 @@
+const projects = [
+    {
+        id: "FF",
+        name: "Friend Finder",
+        description: "",
+        thumbnail: "",
+        url: "",
+        github: "",
+        category: ["group", "javascript", "html", "css", "nodejs"]
+    },
+    {
+        id: "SC",
+        name: "Securify",
+        description: "",
+        thumbnail: "",
+        url: "",
+        github: "",
+        category: ["group", "javascript", "html", "css", "nodejs"]
+    },
+    {
+        id: "CC",
+        name: "Crystal Collector",
+        description: "",
+        thumbnail: "",
+        url: "",
+        github: "",
+        category: ["group", "javascript", "html", "css", "nodejs"]
+    },  {
+        id: "CC",
+        name: "Crystal Collector",
+        description: "",
+        thumbnail: "",
+        url: "",
+        github: "",
+        category: ["group", "javascript", "html", "css", "nodejs"]
+    },
+
+];
